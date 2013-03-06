c4a64b78e0b75e8e80ad4669634fe25905976e44
gragtah <spideyunlimited@gmail.com>
2013-03-06 18:54:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColgateCalendar::Application.config.secret_token = '56064f8008db898ec7a00e759b61438be91a01df36428b19f7d5478f0a5ee1bb2afc0fa56a720c06a5c0429669ca67a53b4887648e0e77a771215c17abc98fdf'
