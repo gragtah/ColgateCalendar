ba710d37497a6b15b056159f683bfdfe6521177c
gragtah <spideyunlimited@gmail.com>
2013-04-01 10:21:31 -0400

    setting up test user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :keywords, :name, :password
+    attr_accessible :email, :tags, :name, :password
     
-def self.authenticated?(username, password)
-    user = find_by_username(username)
-    if user.nil?
-        false
-    elsif user.has_password?(password)
-        true
-    else
-        false
+    def self.authenticated?(username, password)
+        user = find_by_username(username)
+        if user.nil?
+            false
+        elsif user.has_password?(password)
+            true
+        else
+            false
+        end
     end
-end
 end
