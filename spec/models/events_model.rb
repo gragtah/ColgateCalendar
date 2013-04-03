7b144c301c3b12c9f5bbdd482e42e04cab8d9640
Slava <mstislavfedorchuk@gmail.com>
2013-04-02 22:09:52 -0400

    Started writing code for filter by tag function

--- spec/models/events_model.rb
+++ spec/models/events_model.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Event do
-
+	describe "get events after filtering by tags" do
+		session :logged_in = true
+		session :username = "user"
+		fake_user = mock(User, :username => "user") 
+      	tags = User.stub!(:find_by_username).with(session[:username]).tags.downcase.split(','))
+      	
+		it "should show events matching at least one tag" do
+		
+		end
+		
+		it "should not show events not matching any tag" do
+		end
+	end
 end
