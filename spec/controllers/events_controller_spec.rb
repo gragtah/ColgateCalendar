5483ee26cd50841820eb9d108bfae5c4c2c22736
gragtah <spideyunlimited@gmail.com>
2013-04-03 02:07:57 -0400

    all cuke, rspec tests done, 100% coverage

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,18 +1,22 @@
 require 'spec_helper'
 
 describe EventsController do
-
 	describe "events by time" do
+                before(:each) do
+                        session[:logged_in] = true
+                        session[:username] = "user"
+    	        	fake_user = mock(User, :username => "user", :tags => "arts") 
+                        User.stub!(:find_by_username).with("user").and_return(fake_user)
+                        Event.should_receive(:find).with(:all, hash_including(:conditions => anything))
+                end
 		it "should render events today" do
 			get :events_today
 			response.should render_template :events_list
 		end
-
 		it "should render events tomorrow" do
 			get :events_tomorrow
 			response.should render_template :events_list
 		end
-		
 		it "should render events this week" do
 			get :events_this_week
 			response.should render_template :events_list
@@ -20,7 +24,8 @@ describe EventsController do
 	end
 
 	describe "show all events" do
-		it "should show all events (WOW REALLY?!)" do
+		it "should show all events" do
+                        Event.should_receive(:all)
 			get :index
 			response.should render_template :index
 			#Really hope this works, its a super complex function
@@ -29,6 +34,8 @@ describe EventsController do
 	
 	describe "fetch and save events" do
 		it "should fetch and save events" do
+                        Event.should_receive(:create).at_least(1).times
+#Event.should_receive(:fetch_and_save_events)
 			get :fetch_and_save_events
 			response.should render_template :fetch_and_save_events
 			#I actually spent more time trying to think of a witty comment than writing this test. 
