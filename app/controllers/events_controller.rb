55bfe28cf5e351b565130914aaf85bb88cc1dc7e
gragtah <spideyunlimited@gmail.com>
2013-04-14 20:24:54 -0400

    fixed login frontend bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,8 +7,6 @@ class EventsController < ApplicationController
   @@size = 10
   def index
     @events = Event.all
-    @logged_in = session[:logged_in]
-    @username = session[:username]
   end
 
 =begin
