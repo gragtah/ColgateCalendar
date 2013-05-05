989b20c2f40d6a0bffc6c98cb5465958c9cb3877
JackHolland <sirjackholland@gmail.com>
2013-05-05 09:09:48 -0400

    Changed event list layout to a more user friendly and mobile friendly style

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
             session[:admin] = @user.admin
             sign_in_and_redirect @user, :event => :authentication
         else
-             session["devise.google_data"] = request.env["omniauth.auth"]
+            session["devise.google_data"] = request.env["omniauth.auth"]
             redirect_to new_user_registration_url
         end
     end
